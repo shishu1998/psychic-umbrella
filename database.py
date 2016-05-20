033a57e1b0048d6f27b2a5331945aec1d29787ee
StridentStorm <chussean@gmail.com>
2016-05-20 12:56:33 -0400

    Databases

--- database.py
+++ database.py
@@ -1,27 +1,45 @@
 
 from pymongo import MongoClient
-
+from hashlib import sha512
+hidden =""
 def addMap(empireName, date, image):
     #Establish Connection
     connection = MongoClient()
-    c = connection['data']
+    c = connection['data2']
     empName = empireName
     #Create the new row
     d = {'date':date,
-         'image-link':image,
+         'image':image,
         }
     #Add it to the Empire?
+    print d
     c[empName].insert(d)
+    #print c[empName].find_one('date': date)
 
 def getMaps(empireName):
     #Establish Connection
     connection = MongoClient()
-    c = connection['data']
+    c = connection['data2']
     empName = empireName
     #Get Everything and put it in a dictionary
     d = {}
+    array = []
     for x in c[empName].find():
-        d[x.date] = x.image
+       # print x
+        d[x['date']] = x['image']
+        array.append(d)
+        d = {}
     #Return the Dictionary
-    return d
+    
+    print array
+    return array
+    
+#addMap("China","bkad","bdasd")
+getMaps("China")
+
+def regPass(password):
+    salt = uuid4().hex
+    hashvalue = sha512((password + salt)*10000).hexdigest()
+    hidden = hashvalue
+
     
