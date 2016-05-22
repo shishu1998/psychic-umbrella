c939b60c931816a6aba5fd5b06cfc25262010ba0
Nydroam <rayw.u17@gmail.com>
2016-05-22 18:14:36 -0400

    home page js works

--- static/home.js
+++ static/home.js
@@ -1,31 +1,56 @@
 //dict of continents, will connect them with mongodb
 
 Conts = {};
-Conts["AF"]=[];
-Conts["AS"]=[];
-Conts["EU"]=[];
-Conts["NA"]=[];
-Conts["OC"]=[];
-Conts["SA"]=[];
+Conts["AF"]=["Libya","Kenya","South Africa"];
+Conts["AS"]=["China","Korea","Japan","Vietnam"];
+Conts["EU"]=["Britain","France","Germany"];
+Conts["NA"]=["United States","Canada","Mexico","a","b","c","d","e","a","b","c","d","e","a","b","c","d","e"];
+Conts["OC"]=["Australia","Philippines"];
+Conts["SA"]=["Maya","Inca","Brazil"];
 
 var popup = document.getElementById("container");
 var currCont = null;
 d3.selectAll("g").on("click", function() {
     var newCont = this;
-    console.log(this);
     if(currCont == newCont){
-	container.innerHTML="";
+	popup.innerHTML="";
 	currCont = null;
+    popup.style.pointerEvents="none";
     }
     else{
 	currCont = newCont;
-	createPop(currCont.className);
+	createPop(currCont.className.baseVal);
     }
 });
 
+
 //creates the popup screen when a continent is clicked
+var container = d3.select("#container");
 function createPop(c){
+    //resets container
     popup.innerHTML = "";
+
+    //set up return button
+    container.append("div")
+    .attr("class","empReturn")
+    .append("label")
+    .attr("class","unselectable")
+    .text("Return");
+    d3.select(".empReturn").on("click", function(){
+    popup.innerHTML="";
+    currCont = null;
+    container.style("pointer-events","none");
+    });
+
+    //set up empire list
+    var empires = container.selectAll("div")
+    .data(Conts[c])
+    .enter()
+    .append("div")
+    .attr("class","empSelect")
+    .append("label")
+    .attr("class","unselectable")
+    .text(function(d){return d;});
+    container.style("pointer-events","auto");
+
 }
-
-
