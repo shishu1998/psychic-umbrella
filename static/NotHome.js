94ca1324aa48156dd09e56f19020c2567f8fd55e
Shi Shu <shishu_1998@yahoo.com>
2016-05-22 18:41:22 -0400

    test

--- static/NotHome.js
+++ static/NotHome.js
@@ -1,6 +1,7 @@
 var slidenum = 0;
 var body = document.body;
 var elements = document.body.innerHTML.split("/");
+/*
 var dates = elements[0].split(" ");
 var maps = elements[1].split(" ");
 
@@ -12,3 +13,4 @@ var update = function(slide){
 console.log(dates);
 console.log(maps);
 window.addEventListener("load",update(0));
+*/
