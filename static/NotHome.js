b2dcdc104dcba6e51e3c201a3ca0e74745264a6c
shishu1998 <shishu_1998@yahoo.com>
2016-05-22 18:18:40 -0400

    test

--- static/NotHome.js
+++ static/NotHome.js
@@ -1,9 +1,14 @@
 var slidenum = 0;
 var body = document.body;
+var elements = document.body.innerHTML.split("/");
+var dates = elements[0].split(" ");
+var maps = elements[1].split(" ");
 
-
-var update = function(slidenum){
-    date.innerHTML = data[slidenum];
-    map.src = data[slidenum];
+var update = function(slide){
+    slidenum = slide;
+    body.innerHTML = maps[slidenum] + "<br>" + dates[slidenum];
 };
 
+console.log(dates);
+console.log(maps);
+window.addEventListener("load",update(0));
