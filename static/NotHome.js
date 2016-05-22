5ec16f18e644732cb0c8cd63d459b22431b468d3
Shi Shu <shishu_1998@yahoo.com>
2016-05-22 18:09:37 -0400

    stuff

--- static/NotHome.js
+++ static/NotHome.js
@@ -1,5 +1,7 @@
-var date = document.getElementById("date");
-var map = document.getElementById("map");
+var slidenum = 0;
+var body = document.body;
+
+
 var update = function(slidenum){
     date.innerHTML = data[slidenum];
     map.src = data[slidenum];
