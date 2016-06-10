8363ca8b537cd0c56f013fa98ca39321fc1880dc
Nydroam <rayw.u17@gmail.com>
2016-06-10 11:14:44 -0400

    fixed css and added in world map

--- setup.py
+++ setup.py
@@ -1,2 +1,7 @@
+from pymongo import MongoClient
 import database
+connection = MongoClient()
+connection.drop_database("data3")
 database.regPass("worldhistoryrules")
+c = connection["data3"]
+world = c["World"]
